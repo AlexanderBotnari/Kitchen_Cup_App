a2cb92c4866515feba9ce1fdb6746bade00e7a50
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-05-01 13:21:52 +0300

    Kitchen

---
+++ Cup.java
@@ -0,0 +1,34 @@
+package Kitchen;
+
+public class Cup {
+	
+	private String liquidName;
+    private int liquidVolume;
+    
+    public  Cup(){}
+    public Cup( String liquidName, int liquidVolume ){
+        setLiquidName(liquidName);
+        setLiquidVolume(liquidVolume);
+    }
+	public String getLiquidName() {
+		return liquidName;
+	}
+	public void setLiquidName(String liquidName) {
+		if(liquidName == "Milk" || liquidName == "Water" || liquidName == "Tea") {
+			this.liquidName = liquidName;
+		}else {
+			System.err.println("Invalid liquid name !");
+		}
+	}
+	public int getLiquidVolume() {
+		return liquidVolume;
+	}
+	public void setLiquidVolume(int liquidVolume) {
+		if(liquidVolume >= 1 && liquidVolume <= 600) {
+			this.liquidVolume = liquidVolume;
+		}else {
+			System.err.println("Incorect liquid volume value !\n"
+					+ "Please enter 1 to 600 ml");
+		}
+	}
+}
