c49906c04f50c663a54901a7f55d4920378e7fbf
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-05-03 04:03:44 -0700

    Update SmallCup.java

--- src/Kitchen/SmallCup.java
+++ src/Kitchen/SmallCup.java
@@ -4,12 +4,11 @@ public class SmallCup extends Cup{
 	
     public SmallCup() {}
 	public SmallCup( String liquidName, int liquidVolume) {
-	    setLiquidVolume(liquidVolume);
-	    setLiquidName(liquidName);
+	  super(liquidName, liquidVolume);
 	}
 	public void setLiquidVolume(int liquidVolume) {
     	if(liquidVolume >= 0 && liquidVolume <= 250) {
-    		super.setLiquidVolume(liquidVolume);
+    		this.liquidVolume = liquidVolume;
     	}else {
     		System.err.println("Incorect liquid volume value !\n"
 					+ "Please enter 0 to 250 ml");
